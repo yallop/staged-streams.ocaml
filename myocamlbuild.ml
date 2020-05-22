4a0a6a22eaa02b3239d0f54246db40efaf0f7be6
Jeremy Yallop <yallop@gmail.com>
2020-05-22 14:27:24 +0100

    Add support for MetaOCaml N107

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -2,3 +2,17 @@
 (* OASIS_STOP *)
 Ocamlbuild_plugin.mark_tag_used "tests";;
 Ocamlbuild_plugin.dispatch dispatch_default;;
+
+open Ocamlbuild_plugin;;
+open Ocamlbuild_pack;;
+
+dispatch begin
+  function
+  | After_rules ->
+     flag ["ocaml"; "compile"; "open_print_code"] &
+     if Sys.ocaml_version = "4.04.0" then S[A"-open"; A"Print_code"]
+     else if Sys.ocaml_version = "4.07.1" then S[A"-open"; A"Codelib"]
+     else Printf.kprintf failwith "Unsupported OCaml version %s" Sys.ocaml_version
+  | _ -> ()
+end;;
+
